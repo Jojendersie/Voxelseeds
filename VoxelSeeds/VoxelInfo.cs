7f134fd96ab4f916624aea9b5550e408eb558f66
Johannes Jendersie <jojendersie@web.de>
2013-06-14 23:18:14 +0200

    Interfaces/Basics for communication of voxel properties

---
+++ VoxelSeeds/VoxelInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoxelSeeds
+{
+    enum VoxelType
+    {
+        GROUND,
+        WOOD,
+        FUNGUS
+    };
+
+    struct VoxelInfo
+    {
+        /// <summary>
+        /// Create a new voxel. A new voxel is always in living state.
+        /// </summary>
+        /// <param name="type">A type of the enumeration type. Do not used more than
+        /// 128 different types in the enumeration.</param>
+        public VoxelInfo(VoxelType type, bool living = true, int resources = 0)
+        {
+            System.Diagnostics.Debug.Assert( (byte)type < 128 );
+            // First bit == living or dead
+            _type = type;
+            _living = living;
+            _resources = resources;
+        }
+
+        public bool Living { get { return _living; } private set { _living = value; } }
+        public VoxelType Type { get { return _type; } }
+        public int Resources { get { return _resources; } }
+
+        VoxelType _type;
+        bool _living;
+        int _resources;
+    }
+}
