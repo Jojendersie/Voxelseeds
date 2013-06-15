85ae40249b63505ade7631e6d1b52b8617635b41
Johannes Jendersie <jojendersie@web.de>
2013-06-15 10:15:15 +0200

    Map interface extended

--- VoxelSeeds/Map.cs
+++ VoxelSeeds/Map.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace VoxelSeeds
 {
+    /// <summary>
+    /// The map is a 3D array of byte values.
+    /// 
+    /// Later the map gets a loader or generator for the different levels.
+    /// 
+    /// The most significant bit encodes if the voxel is living or dead.
+    /// </summary>
     class Map
     {
         public Map(int sizeX, int sizeY, int sizeZ)
@@ -26,9 +34,41 @@ namespace VoxelSeeds
         readonly int _sizeY;
         readonly int _sizeZ;
 
+        /// <summary>
+        /// Returns a unique number for each position.
+        /// </summary>
+        /// <param name="x">Voxel postion x</param>
+        /// <param name="y">Voxel postion y</param>
+        /// <param name="z">Voxel postion z</param>
+        /// <returns>The PositionCode containting every information for the
+        /// position.</returns>
+        public int EncodePosition(int x, int y, int z)
+        {
+            Debug.Assert((0 <= x) && (x < _sizeX));
+            Debug.Assert((0 <= y) && (x < _sizeY));
+            Debug.Assert((0 <= z) && (x < _sizeZ));
+            return x+SizeX*(y+SizeY*z);
+        }
+
+        /// <summary>
+        /// Calculates the original position from a position code.
+        /// 
+        /// This direction is slower than EncodePosition.
+        /// </summary>
+        /// <param name="positionCode"></param>
+        /// <param name="x">Output of position x</param>
+        /// <param name="y">Output of position y</param>
+        /// <param name="z">Output of position z</param>
+        public void DecodePosition(int positionCode, out int x, out int y, out int z)
+        {
+            x = positionCode % SizeX;
+            y = (positionCode / SizeX) % SizeY;
+            z = positionCode / (SizeX * SizeY);
+        }
+
         public byte Sample(int x, int y, int z)
         {
-            throw new NotImplementedException();
+            return _voxels[EncodePosition(x, y, z)];
         }
     }
 }
