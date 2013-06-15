bf4921925adf8491fb99456e8d5c46698d9fa852
gebert <gebertenrico@googlemail.com>
2013-06-15 21:03:30 +0200

    next tree rule

--- VoxelSeeds/Rules/SpruceWoodRule.cs
+++ VoxelSeeds/Rules/SpruceWoodRule.cs
@@ -17,24 +17,28 @@ namespace VoxelSeeds.Rules
             int gen = neighbourhood[1, 1, 1].Generation;
             if (gen == 0)
             {
-                // Grow Cross and ovveride current generation
-                output[1, 1, 1] = new VoxelInfo(VoxelType.PINE_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.SPRUCE_WOOD) / 2);
-                output[2, 1, 1] = new VoxelInfo(VoxelType.PINE_WOOD, true, 1);
-                output[0, 1, 1] = new VoxelInfo(VoxelType.PINE_WOOD, true, 1);
-                output[1, 1, 2] = new VoxelInfo(VoxelType.PINE_WOOD, true, 1);
-                output[1, 1, 0] = new VoxelInfo(VoxelType.PINE_WOOD, true, 1);
+                // Grow 3x3 and ovveride current generation
+                output[1, 1, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.SPRUCE_WOOD) / 2);
+                output[2, 1, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.SPRUCE_WOOD) / 4);
+                output[0, 1, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.SPRUCE_WOOD) / 4);
+                output[1, 1, 2] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.SPRUCE_WOOD) / 4);
+                output[1, 1, 0] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.SPRUCE_WOOD) / 4);
+                output[2, 1, 2] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1);
+                output[2, 1, 0] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1);
+                output[0, 1, 2] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1);
+                output[0, 1, 0] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, 1);
             }
             else if (gen < 9)
             {
                 // Grow upwards
-                output[1, 2, 1] = new VoxelInfo(VoxelType.PINE_WOOD, true, gen + 1);
-                output[1, 1, 1] = new VoxelInfo(VoxelType.PINE_WOOD);
+                output[1, 2, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD, true, gen + 1);
+                output[1, 1, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD);
             }
             else
             {
                 // Grow upwards one last time
-                output[1, 2, 1] = new VoxelInfo(VoxelType.PINE_WOOD);
-                output[1, 1, 1] = new VoxelInfo(VoxelType.PINE_WOOD);
+                output[1, 2, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD);
+                output[1, 1, 1] = new VoxelInfo(VoxelType.SPRUCE_WOOD);
             }
             return output;
         }
