83a793d9cf6447156b506e3745df7d307a6c7d1f
gebert <gebertenrico@googlemail.com>
2013-06-15 21:48:46 +0200

    growing oak

--- VoxelSeeds/Rules/OakWoodRule.cs
+++ VoxelSeeds/Rules/OakWoodRule.cs
@@ -15,14 +15,46 @@ namespace VoxelSeeds.Rules
 
             VoxelInfo[, ,] output = new VoxelInfo[3, 3, 3];
             int gen = neighbourhood[1, 1, 1].Generation;
+            int res = neighbourhood[1, 1, 1].Resources;
             if (gen == 0)
             {
                 // Grow Cross and ovveride current generation
                 output[1, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.OAK_WOOD) / 2);
-                output[2, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
-                output[0, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
-                output[1, 1, 2] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
-                output[1, 1, 0] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
+                output[2, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 10);
+                output[0, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 10);
+                output[1, 1, 2] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 10);
+                output[1, 1, 0] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 10);
+            }
+            else if (gen == 1 && res == 10)
+            {
+                // Grow Cross and ovveride current generation
+                output[1, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.OAK_WOOD) / 2);
+                if (neighbourhood[2, 1, 1].Type == VoxelType.EMPTY)
+                    output[2, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 20);
+                if (neighbourhood[0, 1, 1].Type == VoxelType.EMPTY)
+                    output[0, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 20);
+                if (neighbourhood[1, 1, 2].Type == VoxelType.EMPTY)
+                    output[1, 1, 2] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 20);
+                if (neighbourhood[1, 1, 0].Type == VoxelType.EMPTY)
+                    output[1, 1, 0] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 20);
+            }
+            else if (gen == 1 && res == 20)
+            {
+                output[1, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1, 0, TypeInformation.GetGrowingSteps(VoxelType.OAK_WOOD) / 4);
+                if (neighbourhood[1, 1, 0].Type != VoxelType.OAK_WOOD && neighbourhood[1, 1, 2].Type != VoxelType.OAK_WOOD)
+                {
+                    if (neighbourhood[1, 1, 2].Type == VoxelType.EMPTY)
+                        output[1, 1, 2] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
+                    if (neighbourhood[1, 1, 0].Type == VoxelType.EMPTY)
+                        output[1, 1, 0] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
+                }
+                else if (neighbourhood[0, 1, 1].Type != VoxelType.OAK_WOOD && neighbourhood[2, 1, 1].Type != VoxelType.OAK_WOOD)
+                {
+                    if (neighbourhood[2, 1, 1].Type == VoxelType.EMPTY)
+                        output[2, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
+                    if (neighbourhood[0, 1, 1].Type == VoxelType.EMPTY)
+                        output[0, 1, 1] = new VoxelInfo(VoxelType.OAK_WOOD, true, 1);
+                }
             }
             else if (gen < TypeInformation.GetGrowHeight(VoxelType.OAK_WOOD))
             {
